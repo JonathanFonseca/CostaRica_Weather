d6f8e03b8307c30284b087beadba833bf868fafb
JonathanFonseca <67702120+JonathanFonseca@users.noreply.github.com>
2021-04-20 01:07:17 -0600

    Se remueve app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import './sass/styles.scss'
 import MainContainer from './container/Weather/mainContainer';
 import Footer from './components/footer'
